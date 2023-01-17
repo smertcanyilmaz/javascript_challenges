13650631a0d00e7dd8c8015e60dbcf4542625409
Mertcan Yılmaz <107379154+smertcanyilmaz@users.noreply.github.com>
2023-01-17 08:29:34 +0300

    Add files via upload

---
+++ bmi calculator/script.js
@@ -0,0 +1,13 @@
+
+const markMass= 78;
+const markHeight= 169;
+const markBmi = markMass/markHeight**2;
+console.log(markBmi);
+
+const johnMass= 95;
+const johnHeight= 188;
+const johnBmi = johnMass/johnHeight**2;
+console.log(johnBmi);
+
+const markHigherBMI = markBmi > johnBmi;
+console.log( markHigherBMI);
